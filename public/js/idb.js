ec8b29b7a505b7d32bc7961135105e4b043a66ad
christian <100375218+chriscarv@users.noreply.github.com>
2022-08-07 17:21:36 -0400

    maskable icon

---
+++ public/js/idb.js
@@ -0,0 +1,11 @@
+let db;
+const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore('new_budget', {autoIncrement: true});
+};
+
+request.onsuccess = function(event) {
+    
+}
